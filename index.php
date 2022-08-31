44c7a271d6b10b7ae3c0a16122a91130d010a612
Fernweh99 <daniele.dimento@gmail.com>
2022-08-31 16:22:21 +0200

    finish main

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     "Come state implementando la recente decisione della Corte di giustizia 
     dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [
       'answers' => [
-        "0" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze 
+        "0" => "La recente 
+        <a href='https://curia.europa.eu/juris/document/document.jsf?text=&docid=152065&amppageIndex=0&doclang=it&mode=lst&dir=&occ=first&part=1&cid=276332'>
+        decisione della Corte di giustizia dell'Unione europea </a> ha profonde conseguenze 
         per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto 
         di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di 
         ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati 
@@ -14,8 +16,9 @@
         valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a 
         controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
 
-        "2" => "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta 
-        automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso 
+        "2" => "Per presentare una richiesta di rimozione, compila questo 
+        <a href='https://www.google.com/webmasters/tools/legal-removal-request?complaint_type=rtbf&visit_id=637975521848746075-4236018462&hl=it&rd=1'> 
+        modulo web </a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso 
         (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). 
         Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla 
         tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei 
@@ -47,19 +50,24 @@
         e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
 
         "2" => "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te 
-        e la tua famiglia online, nel Centro Google per la sicurezza online.",
+        e la tua famiglia online, <a href='https://safety.google/'>nel Centro Google per la sicurezza online</a>.",
 
-        "3" => "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+        "3" => "<a href='https://safety.google/'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
       ]
     ],
     "Perché il mio account è associato a un paese?" => [
       "answers" => [
         "0" => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
-        La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy 
+        <ol>
+        <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy 
         vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-        Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, 
-        Liechtenstein e Norvegia) o in Svizzera. Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+        <ol>
+        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, 
+        Liechtenstein e Norvegia) o in Svizzera.
+        </li><li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+        </li></ol></li>
+        <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li> 
+        </ol>",
 
         "1" => "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata 
         che li offre o dal paese a cui è associato il tuo account.",
@@ -74,28 +82,28 @@
 
         "4" => "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza 
         tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo 
-        IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.",
+        IP oppure la residenza vicino a un confine territoriale. <a href='https://policies.google.com/country-association-form'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.",
       ]
     ],
     "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?" => [
       "answers" => [
         "0" => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono 
         rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti 
-        dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli 
+        dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='https://support.google.com/websearch/answer/9109?hl=it'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli 
         di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di 
-        determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati 
+        determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='https://www.google.com/webmasters/tools/legal-removal-request?complaint_type=rtbf&visit_id=637975523148527837-1563012462&hl=it&rd=1'>fai clic qui</a>. Una volta che i contenuti saranno stati 
         rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. 
-        In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+        In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='https://support.google.com/websearch/troubleshooter/3111061?hl=it'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>."
       ]
     ],
     "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?" => [
       "answers" => [
         "0" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina 
-        web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL 
+        web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='https://policies.google.com/privacy/key-terms#toc-terms-referrer-url'>URL referrer</a>. Talvolta, l'URL 
         della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione 
         di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL 
         referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla 
-        ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante 
+        ricerca SSL sono disponibili <a href='https://support.google.com/websearch/answer/173733'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante 
         Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative 
         all' esatte parole chiave che hanno determinato il clic su un annuncio."
       ]
